6daef7931b81bd2e4ec321d3bc9cd222511699ae
Alferdize <simonandrew132@gmail.com>
2020-01-15 01:21:22 +0530

    Model Validation

--- Linear_regression.py
+++ Linear_regression.py
@@ -5,6 +5,8 @@ Linear_regression.py
 from numpy import *
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 
 diamond_path = './diamond.csv'
 
@@ -16,12 +18,48 @@ y = diamond_data.price
 diamond_features = ['carat','depth']
 X = diamond_data[diamond_features]
 
-diamond_model = DecisionTreeRegressor(random_state=1)
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 
-diamond_model.fit(X, y)
+diamond_model = DecisionTreeRegressor()
+
+diamond_model.fit(train_X, train_y)
+
+val_predictions = diamond_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
 
 
-print("Making predictions for the following 5 houses:")
-print(X.head())
-print("The predictions are")
-print(diamond_model.predict(X.head()))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# diamond_model = DecisionTreeRegressor(random_state=1)
+
+# diamond_model.fit(X, y)
+# predicted_diamond_prices = diamond_model.predict(X)
+# mean = mean_absolute_error(y, predicted_diamond_prices)
+# print("Mean :: ",mean)
+# print("Making predictions for the following 5 houses:")
+# print(X.head())
+# print("The predictions are")
+# print(diamond_model.predict(X.head()))
