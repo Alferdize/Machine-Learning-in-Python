452f91a07e8dd1021410e4141976416b30d9d5a0
Alferdize <simonandrew132@gmail.com>
2020-01-16 01:50:40 +0530

    Random forest

--- Linear_regression.py
+++ Linear_regression.py
@@ -5,6 +5,7 @@ Linear_regression.py
 from numpy import *
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor 
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -20,12 +21,82 @@ X = diamond_data[diamond_features]
 
 train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
 
-diamond_model = DecisionTreeRegressor()
 
-diamond_model.fit(train_X, train_y)
 
-val_predictions = diamond_model.predict(val_X)
-print(mean_absolute_error(val_y, val_predictions))
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+#     model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+#     model.fit(train_X, train_y)
+#     preds_val = model.predict(val_X)
+#     mae = mean_absolute_error(val_y, preds_val)
+#     return(mae)
+
+
+
+# for max_leaf_nodes in [5, 50, 500, 5000]:
+#     my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+#     print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# diamond_model = DecisionTreeRegressor()
+
+# diamond_model.fit(train_X, train_y)
+
+# val_predictions = diamond_model.predict(val_X)
+# print(mean_absolute_error(val_y, val_predictions))
 
 
 
