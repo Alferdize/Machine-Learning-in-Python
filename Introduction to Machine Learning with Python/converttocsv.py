70e12c2b223b320ce266ecd2c795b0909a538b60
Alferdize <simonandrew132@gmail.com>
2020-02-26 23:46:58 +0530

    Arff to Csv

---
+++ Introduction to Machine Learning with Python/converttocsv.py
@@ -0,0 +1,34 @@
+import os
+
+
+files = [arff for arff in os.listdir(".") if arff.endswith(".arff")]
+
+
+def toCsv(content):
+    data = False
+    header = ""
+    newContent = []
+    for line in content:
+        if not data:
+            if "@ATTRIBUTE" in line:
+                attri = line.split()
+                columnName = attri[attri.index("@ATTRIBUTE")+1]
+                header = header + columnName + ","
+
+            elif "@DATA" in line:
+                data = True
+                header = header[:-1]
+                header += '\n'
+                newContent.append(header)
+        else:
+            newContent.append(line)            
+    return newContent
+
+
+for file in files:
+    with open(file,"r") as inFile:
+        content = inFile.readlines()
+        name,ext = os.path.splitext(inFile.name)
+        new = toCsv(content)
+        with open(name+".csv","w") as outFile:
+            outFile.writelines(new)
